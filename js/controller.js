47f31188b384470f692ab08d909627afa5667f92
matproulx <matproulx@gmail.com>
2016-03-21 22:02:17 -0400

    Added Slider and categories

--- js/controller.js
+++ js/controller.js
@@ -2,10 +2,30 @@
  * @author MProulx
  */
 
+var _keywords = "";
+var _TOKEN = "6318103b-f9da-437c-854b-9e6f1f44e27b";
+var _URL = "https://cloudplatform.coveo.com/rest/search?access_token=";
+
 function initTest() {
 	$("#Search").click(function(){
-    	$.get("https://cloudplatform.coveo.com/rest/search?access_token=6318103b-f9da-437c-854b-9e6f1f44e27b&q=@tpcepagenomsplitgroup==Merlot", function(data, status){
-    		showResults(data);
-    	});
+    	getResults();
     });
+}
+
+function getResult() {
+	$.get(_URL + _TOKEN + "&q=" + "@tpcepagenomsplitgroup==Merlot", function(data, status){
+		showResults(data);
+	});
+}
+
+function getPrice() {
+	
+}
+
+function getKeywords() {
+	var keywords = $("#keywords").val();
+}
+
+function getCategories() {
+	var categories = "";
 }
--- js/view.js
+++ js/view.js
@@ -2,6 +2,30 @@
  * @author MProulx
  */
 
+function initPriceSlider() {
+	var slider = $("#kendoSliderPrice").kendoRangeSlider({
+        change: rangeSliderOnChange,
+        slide: rangeSliderOnSlide,
+        min: 0,
+        max: 300,
+        smallStep: 10,
+        largeStep: 100,
+        tickPlacement: "both"
+    });
+    
+    var slider = $("#kendoSliderPrice").getKendoRangeSlider();
+    slider.wrapper.css("width", "150px");
+    slider.resize();
+}
+
+function rangeSliderOnSlide(e) {
+    //kendoConsole.log("Slide :: new slide values are: " + e.value.toString().replace(",", " - "));
+}
+
+function rangeSliderOnChange(e) {
+    //kendoConsole.log("Change :: new values are: " + e.value.toString().replace(",", " - "));
+}
+
 function showResults(products) {
 	var dataSource = new kendo.data.DataSource({
         data: products.results,
